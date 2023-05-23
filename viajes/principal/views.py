51a2f8f0d78c80f5615fa3bf018d6f93b16ad3db
Valentin <xnitezl@gmail.com>
2023-05-23 14:15:25 +0200

    update 3

--- viajes/principal/views.py
+++ viajes/principal/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+#from django.shortcuts import get_list_or_404, render
+from .models import viajes
+#from django.http import HttpResponse
+#from django.template import loader
+#from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse, reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin
 
-# Create your views here.
+class ViajesListView(ListView):
+    model = Viajes
